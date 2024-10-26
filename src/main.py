ec71f1317dc85f02e9c1368b4c7f87035f8e83ae
Celso WSL <celsovinicius4@gmail.com>
2024-10-26 20:04:13 -0300

    test[etapa3]: testando execução da etapa 3

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from etapa1_fuzzificacao import executar_etapa1
 from etapa2_particoes import executar_etapa2
+from etapa3_operacoes import executar_etapa3
 
 if __name__ == "__main__":
     # executar_etapa1() # Fuzzificação básica
-    executar_etapa2() # Partições uniformes
+    # executar_etapa2() # Partições uniformes
+    executar_etapa3()
