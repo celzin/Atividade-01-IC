6d23c783b5c3bd1abe46d05b5c4d472c9341a3f8
Celso WSL <celsovinicius4@gmail.com>
2024-10-27 18:40:56 -0300

    test[etapa5]: testando etapa 5 na main

--- src/main.py
+++ src/main.py
@@ -2,9 +2,11 @@ from etapa1_fuzzificacao import executar_etapa1
 from etapa2_particoes import executar_etapa2
 from etapa3_operacoes import executar_etapa3
 from etapa4_relacoes import executar_etapa4
+from etapa5_composicoes import executar_etapa5
 
 if __name__ == "__main__":
     # executar_etapa1() # Fuzzificação básica
     # executar_etapa2() # Partições uniformes
     # executar_etapa3()
-    executar_etapa4()
+    # executar_etapa4()
+    executar_etapa5()
