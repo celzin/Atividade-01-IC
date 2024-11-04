6567cb9fd3f29e48f1b85721a2efe4f9c62e1d6e
Celso WSL <celsovinicius4@gmail.com>
2024-11-04 16:51:55 -0300

    refactor[main]: removendo comentários

--- src/main.py
+++ src/main.py
@@ -5,8 +5,8 @@ from etapa4_relacoes import executar_etapa4
 from etapa5_composicoes import executar_etapa5
 
 if __name__ == "__main__":
-    # executar_etapa1() # Fuzzificação básica
-    # executar_etapa2() # Partições uniformes
-    # executar_etapa3() # Operações fuzzy 
-    # executar_etapa4() # Relações fuzzy
+    executar_etapa1() # Fuzzificação básica
+    executar_etapa2() # Partições uniformes
+    executar_etapa3() # Operações fuzzy 
+    executar_etapa4() # Relações fuzzy
     executar_etapa5() # Composições fuzzy
